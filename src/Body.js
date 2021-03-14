f6f2e9e9881c0ae12e4cdd6e1905e9b78a641ac3
Harsh-Vashistha <45832558+Harsh-Vashistha@users.noreply.github.com>
2021-03-14 23:24:11 +0530

    created project

---
+++ src/Body.js
@@ -0,0 +1,12 @@
+import './Body.css'
+
+import React from 'react'
+function Body() {
+    return (
+            <div className="body">
+                <h1>hello world</h1>
+            </div>       
+    )
+}
+
+export default Body
